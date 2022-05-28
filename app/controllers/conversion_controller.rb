75def75dfde941572fddc699d3b47f36cc49bb1b
Will Stephenson <will.stephenson@gmail.com>
2022-05-28 23:06:37 +0200

    Hook up convert ctrlr to db

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,14 +1,22 @@
 class ConversionController < ApplicationController
   def convert
-    dummy_rate = BigDecimal("1.5")
+    most_recent_quoteset = QuoteSet.order(:timestamp).last!
 
-    # do the conversion here
-    render json: (BigDecimal(convert_params['amount']) * dummy_rate).to_s('F')
+    rate_code = "#{convert_params['source_currency']}#{convert_params['destination_currency']}"
+
+    quote = most_recent_quoteset.quotes.find_by!(currencies: rate_code)
+
+    render json: quote.convert(BigDecimal(convert_params['amount'])).to_s('F')
+  rescue ActiveRecord::RecordNotFound
+    render json:{ error: "rate for currency pair '#{rate_code}' not found", status: :unprocessable_entity}
+  rescue Quote::InconvertibleAmount, ArgumentError
+    render json:{ error: "amount '#{convert_params['amount']}' doesn't look like a currency amount", status: :unprocessable_entity}
+
   end
 
   private
 
   def convert_params
-    params.permit([:amount, :source, :currency])
+    params.permit([:amount, :source_currency, :destination_currency])
   end
 end
