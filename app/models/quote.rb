1ee6f3378d511af011ef6480669dcbc3b6db6358
Will Stephenson <will.stephenson@gmail.com>
2022-05-28 23:06:12 +0200

    Convert method on quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,10 @@
 class Quote < ApplicationRecord
   belongs_to :quote_set
+
+  class InconvertibleAmount < StandardError; end
+
+  def convert(amount)
+    raise InconvertibleAmount unless amount.is_a? BigDecimal
+    rate * amount
+  end
 end
